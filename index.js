ae017e1c0320b7d1fc0f05410fcca673c2c99ab2
Samuel Lozano <Samfcb89@gmail.com>
2026-08-30 17:04:18 +0200

    correccion tercer workflow 2

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ try {
   const conexion = await mysql.createConnection({
     host: process.env.DB_HOST, 
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT 
   });
   console.log('Conexión exitosa a MySQL ✅');
